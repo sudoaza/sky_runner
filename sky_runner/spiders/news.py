455606e5afc31bb320b09ea375b694c8d695954b
aza <aza@lainventoria.com.ar>
2019-10-25 17:31:11 +0200

    changes

--- sky_runner/spiders/news.py
+++ sky_runner/spiders/news.py
@@ -10,18 +10,33 @@ class NewsSpider(scrapy.Spider):
     name = 'news'
 
     start_urls = [
-
+        'https://phys.org/search/?search=3d+printing',
+        'https://techcrunch.com/tag/3d-printing/',
+        'https://www.wired.com/search/?q=3d%20printing&page=1&sort=score',
+        'http://discovermagazine.com/search?q=3d+printing',
+        'https://www.technologyreview.com/search/?s=3d+printing',
+        'https://www.popularmechanics.com/search/?q=3d+printing',
+        'https://www.americanscientist.org/search/node/3d%20printing',
+        'https://www.fastcompany.com/search?searchkey=3d%20printing',
+        'https://www.techradar.com/search?searchTerm=3d+printing',
+        'https://3dprinting.com/',
+        'https://3dprint.com/',
+        'https://3dprintingindustry.com/',
+        'https://all3dp.com/',
+        'https://www.3dnatives.com/',
+        'https://www.3ders.org/',
+        'https://www.3dmake.de/'
         ]
     allowed_domains = []
     allowed_domains.extend([urlparse(url).netloc for url in start_urls])
 
-    maxdepth = 1 # max depth for normal pages
-    maxsearchdepth = 10 # max depth for search pages or indexes
+    maxdepth = 2 # max depth for normal pages
+    maxsearchdepth = 20 # max depth for search pages or indexes
 
     def parse(self, response):
         
 
-        if not self.index_page(response):
+        if not self.index_page(response) and response.url not in self.start_urls:
             article = Article(response.url)
             article.set_html(response.text)
             article.parse()
@@ -33,9 +48,10 @@ class NewsSpider(scrapy.Spider):
             item['title'] = article.title
             if isinstance(article.publish_date, datetime.datetime):
                 item['publish_date'] = article.publish_date.isoformat()
-            item['keywords'] = article.keywords
+            item['keywords'] = ', '.join(article.keywords)
             item['summary'] = article.summary
             item['body'] = article.text
+            item['doctype'] = 'article'
 
             r = requests.post('http://localhost:3000/submit', json={'document': item})
 
